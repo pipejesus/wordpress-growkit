b0bae18846ec83724bc8c7ad2308f0c7a5d8f132
pipejesus <grzegorz.karol.jasinski@gmail.com>
2016-08-08 13:41:17 +0200

    quick round up before a little break for a next project, growkit is frozen for a while ;)

--- page.php
+++ page.php
@@ -1,11 +1,9 @@
 <?php get_header(); ?>
-
 <div class="grow-container constrained">
 	<div class="grow">
 		<div class="grow-item-xs-12">
-			index.php
+			page.php
 		</div>
 	</div>
 </div>
-
 <?php get_footer(); ?>
